8dc7c0cec916bf02cc011149d637112f850b3ad1
Xiong Zhi <62173073+xiongzhilim1@users.noreply.github.com>
2022-08-23 23:40:54 +0800

    Update LayoutEffectTutorial.js

--- src/UseLayoutEffect/LayoutEffectTutorial.js
+++ src/UseLayoutEffect/LayoutEffectTutorial.js
@@ -3,6 +3,7 @@ import { useLayoutEffect, useEffect, useRef } from "react";
 function LayoutEffectTutorial() {
   const inputRef = useRef(null);
 
+  // uselayouteffect hook loads first, whereas useeffect hooks loads and re-renders upon each state changes
   useLayoutEffect(() => {
     console.log(inputRef.current.value);
   }, []);
