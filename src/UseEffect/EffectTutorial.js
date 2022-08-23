4f0c40324b82f64786a010534a218104a31c8ebb
Xiong Zhi <62173073+xiongzhilim1@users.noreply.github.com>
2022-08-23 12:33:08 +0800

    Update EffectTutorial.js

--- src/UseEffect/EffectTutorial.js
+++ src/UseEffect/EffectTutorial.js
@@ -2,18 +2,22 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function EffectTutorial() {
-  const [data, setData] = useState("");
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState(""); // initialised as empty string
+  const [count, setCount] = useState(0); // initialised as integer 0
 
+  // while usestate is like a variable + setter, usereducer is like a object + function, useeffect is a function that gets called on first rendering and on subsequent state changes
+  
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/comments")
       .then((response) => {
         setData(response.data[0].email);
+      // the response data is access via response.data -> with extra specification of index 0 and key email
         console.log("API WAS CALLED");
       });
   }, []);
-
+  // can put within [] what kind of states you want to useeffect one, or for it to listen to reduce times of re-rendering when unnecessary e.g. ,[count]
+  
   return (
     <div>
       Hello World
