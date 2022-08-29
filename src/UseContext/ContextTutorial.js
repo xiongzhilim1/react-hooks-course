463694c252212208505a22707ac96290bcc0d0d5
Xiong Zhi <62173073+xiongzhilim1@users.noreply.github.com>
2022-08-29 14:41:18 +0800

    Update ContextTutorial.js

--- src/UseContext/ContextTutorial.js
+++ src/UseContext/ContextTutorial.js
@@ -2,12 +2,16 @@ import React, { useState, createContext } from "react";
 import Login from "./Login";
 import User from "./User";
 
+// createcontext (i.e. contexthooks) are a collection of states and setstates function that can be accessed as long as the wrapper brings in the states and setters
+
+// declaring the context object, and export so that it can be used by other components
 export const AppContext = createContext(null);
 
 function ContextTutorial() {
   const [username, setUsername] = useState("");
 
   return (
+    // wrapping it and indicate which state / setter can be accessed 
     <AppContext.Provider value={{ username, setUsername }}>
       <Login /> <User />
     </AppContext.Provider>
